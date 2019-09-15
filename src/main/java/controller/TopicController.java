e1cecade01e5e9e37cd328924f6d4371e7debfe2
kamilac11 <kamilachoja@gmail.com>
2019-09-15 23:42:50 +0200

    first commit

---
+++ src/main/java/controller/TopicController.java
@@ -0,0 +1,44 @@
+package controller;
+
+import model.Topic;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import service.TopicService;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class TopicController {
+
+    @Autowired
+    private TopicService topicService; {
+    }
+
+    @RequestMapping("/topics")
+    public List<Topic> getAllTopics() {
+        return topicService.getAllTopics();
+    }
+
+    @RequestMapping("/topics/{id}")
+    public Optional<Topic> getTopic(@PathVariable String id) {
+        return topicService.getTopic(id);
+    }
+
+    //metody z adnotacja RequestMapping Post są void bo nic nie zwracaja a WYSYŁAJĄ
+    @RequestMapping(method = RequestMethod.POST, value = "/topics")
+    public void addTopic(@RequestBody Topic topic){
+        topicService.addTopic(topic);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "/topics/{id}")
+    public void updateTopic(@RequestBody Topic topic, @PathVariable String id) {
+        topicService.updateTopic(id, topic);
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "/topics/{id}")
+    public void deleteTopic(@PathVariable String id) {
+        topicService.deleteTopic(id);
+    }
+
+}
---
+++ src/main/java/repository/TopicRepository.java
@@ -0,0 +1,10 @@
+package repository;
+
+import model.Topic;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TopicRepository extends JpaRepository<Topic, String> {
+
+}
